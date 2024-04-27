4f1cf7bb8e034027fb8d98124078cad1e88239d3
ProbablyCanadian <rbanwart745@gmail.com>
2024-04-27 18:36:46 -0500

    orders backend fixed somehow

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -3,26 +3,23 @@ let orderModel = require('../models/ordersModel');
 
 router.route('/').get((req, res) => {
     orderModel.find()
-      .then(items => res.json(items))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
-  router.route('/add').post((req, res) => {
-    const user_id = req.body.user_id;
-    const designer = req.body.designer;
-    const product_id = req.body.product_id;
-    const total_cost = req.body.total_cost;
-    const numBed = req.body.numBed;
-    const numBath = req.body.numBath;
-    const q = req.body.q;
-    const tags = req.body.tags;
-  
-  
-    const AddtoOrders = new orderModel({user_id,designer,product_id,total_cost,numBed,numBath,q,tags});
-  
-    AddtoOrders.save()
-      .then(() => res.json('Item added to Cart!'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
-  module.exports = router;
+        .then(items => res.json(items))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const itemsToAdd = req.body; // Assuming the request body is an array of items to add
+    const addPromises = [];
+
+    itemsToAdd.forEach(item => {
+        const { user_id, designer, product_id, total_cost, numBed, numBath, q, tags } = item;
+        const AddtoOrders = new orderModel({ user_id, designer, product_id, total_cost, numBed, numBath, q, tags });
+        addPromises.push(AddtoOrders.save());
+    });
+
+    Promise.all(addPromises)
+        .then(() => res.json('Items added to Orders!'))
+        .catch(err => res.status(400).json('Error adding items to Orders: ' + err));
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,16 +3,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser'); // Import bodyParser
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
-
-const bodyParser = require("body-parser")
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // Use bodyParser for urlencoded data
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/CourseProject', {
@@ -22,20 +21,15 @@ mongoose.connect('mongodb://localhost:27017/CourseProject', {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-const cartRouter = require ('./routes/addCart');
-
+const cartRouter = require('./routes/addCart');
 app.use('/addCart', cartRouter);
 
 const structureRoutes = require('./routes/structureRoutes');
+app.use('/api/Structures', structureRoutes);
 
-// Use routes
-app.use("/api/Structures", structureRoutes);
-
-//order routing shit
-const ordersRoutes = require('./routes/ordersRoute');
-
-app.use("/api/orders", ordersRoutes);
-
+// Import and use ordersRoute
+const ordersRouter = require('./routes/ordersRoute');
+app.use('/orders', ordersRouter);
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
